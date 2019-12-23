1bd917a3d086b4abbaf4221787fde74b09db7c0d
Leandro Laurindo <Leandro Laurindo@LeandroLaurindo>
2019-12-23 12:55:52 -0300

    atualização

--- src/main/java/br/com/localeader/api/LocadoraApplication.java
+++ src/main/java/br/com/localeader/api/LocadoraApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LocadoraApplication {
 
+    
 	public static void main(String[] args) {
 		SpringApplication.run(LocadoraApplication.class, args);
 	}
